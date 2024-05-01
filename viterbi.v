a5ff39b0cc898a4e39de6dfe1cf784d308eb5125
Ganesha_G_Rao <130823089+K-Ganesh-Rao@users.noreply.github.com>
2024-05-01 21:37:10 +0530

    Create viterbi.v

---
+++ viterbi.v
@@ -0,0 +1,57 @@
+// Code your design here
+module(
+  input clk,
+  output out
+  
+  
+);
+  reg [1:0]state;
+  reg st;
+  
+  always@(posedge clk)
+    begin
+      case(state)
+        00:
+          begin
+            if(st ==1)begin
+				
+				end
+           else if(st ==0)begin
+				
+				end				
+          end
+          
+        01:
+          begin
+            if(st ==1)begin
+				
+				end
+           else if(st ==0)begin
+				
+				end				
+          end
+        
+        10:
+          begin
+            if(st ==1)begin
+				
+				end
+           else if(st ==0)begin
+				
+				end				
+          end
+          
+        11:
+          begin
+            if(st ==1)begin
+				
+				end
+           else if(st ==0)begin
+				
+				end				
+          end
+    end
+  
+  
+  
+endmodule
